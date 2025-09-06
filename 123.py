a2f18ee93f8d3c99e999e63918c37b92103cf07b
MishKish <jekakudrov@gmail.com>
2025-09-06 13:54:48 +0300

    Переосмысление работы бота

--- 123.py
+++ 123.py
@@ -2,8 +2,8 @@ import sqlite3
 
 conn = sqlite3.connect('bot.db')
 cursor = conn.cursor()
-cursor.execute("UPDATE birthdays SET chat_id = ? WHERE chat_id IS NULL", (YOUR_CHAT_ID,))
+cursor.execute("UPDATE birthdays SET chat_id = ? WHERE chat_id IS NULL", (chat_id,))
 conn.commit()
 conn.close()
 
-print("Записи успешно обновлены!")
+print("Записи обновлены")
